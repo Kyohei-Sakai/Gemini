2689c89ab0ff996bfc5e4b9988ed9ee0cfcd4980
shoheiyokoyama <shohei.yok0602@gmail.com>
2017-06-10 17:14:56 +0900

    Order source directory

---
+++ Gemini/GeminiCollectionView.swift
@@ -0,0 +1,14 @@
+//
+//  GeminiCollectionView.swift
+//  Pods
+//
+//  Created by shoheiyokoyama on 2017/06/10.
+//
+//
+
+import UIKit
+
+final class GeminiCollectionView: UICollectionView {
+
+    
+}
