df23a0145ca4e76db23eb7432a2cca3bb5d3f9c8
shoheiyokoyama <shohei.yok0602@gmail.com>
2017-07-02 15:00:22 +0900

    Reset transform and anchorPoint in prepareForReuse

--- Gemini/GeminiCell.swift
+++ Gemini/GeminiCell.swift
@@ -11,7 +11,10 @@ import UIKit
 open class GeminiCell: UICollectionViewCell {
     override open func prepareForReuse() {
         super.prepareForReuse()
+        //Reset anchorPoint
         adjustAnchorPoint()
+        //Reset CATransform3D
+        layer.transform = CATransform3DIdentity
     }
 
     open var shadowView: UIView? {
